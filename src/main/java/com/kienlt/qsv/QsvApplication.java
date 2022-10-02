0decdba0d3dc64c771fb1ab7bd75f2b345fea64e
kienlt97 <kienlt543@gmail.com>
2022-10-02 08:36:27 +0700

    Khởi tạo project

---
+++ src/main/java/com/kienlt/qsv/QsvApplication.java
@@ -0,0 +1,13 @@
+package com.kienlt.qsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kienlt/qsv/QsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kienlt.qsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
