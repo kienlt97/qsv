7f677f64d6d9a6641c647eb536dcf5adca40ccbb
kienlt97 <kienlt543@gmail.com>
2022-10-02 15:10:33 +0700

    add file jenkin

---
+++ Jenkinsfile
@@ -0,0 +1,14 @@
+pipeline{
+    agent any
+    tools{
+        maven 'Maven 3.8.4'
+    }
+    stages{
+        stage('Build Maven'){
+            steps{
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/kienlt97/qsv']]])
+                bat 'mvn clean install'
+            }
+        }
+    }
+}
--- src/main/java/com/kienlt/qsv/controller/GetProductController.java
+++ src/main/java/com/kienlt/qsv/controller/GetProductController.java
@@ -1,14 +1,14 @@
 package com.kienlt.qsv.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("books")
+@RequestMapping("/books")
 public class GetProductController {
 
-    @GetMapping("/all")
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
     public String getContent(){
         return "Hello toi da goi vao day";
     }
